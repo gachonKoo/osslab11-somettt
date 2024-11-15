51b26e5682059453c18ac63e7265fcf5b9257eb8
somettt <165227931+somettt@users.noreply.github.com>
2024-11-15 21:28:34 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,18 +1,9 @@
-from geo import calculate_square_area, calculate_rectangle_area, calculate_circle_area
+import geo.utils as utils
 
-def test_square_area():
-    assert calculate_square_area(4) == 16
-    print("Square area test passed!")
+a, b = 3, 4
+c = utils.pythagoras(a,b)
+print('c =', c)
 
-def test_rectangle_area():
-    assert calculate_rectangle_area(5, 3) == 15
-    print("Rectangle area test passed!")
-
-def test_circle_area():
-    assert round(calculate_circle_area(3), 2) == 28.27
-    print("Circle area test passed!")
-
-if __name__ == "__main__":
-    test_square_area()
-    test_rectangle_area()
-    test_circle_area()
+r = 10
+area = utils.circle(r)
+print('area =', area)
