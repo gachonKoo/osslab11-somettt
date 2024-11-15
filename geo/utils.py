b25fd437a8994cd8c953f6f6298154ced92bf4af
somettt <165227931+somettt@users.noreply.github.com>
2024-11-15 21:20:34 +0900

    Create utils.py

---
+++ geo/utils.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_square_area(side):
+    return side * side
+
+def calculate_rectangle_area(length, width):
+    return length * width
+
+def calculate_circle_area(radius):
+    return math.pi * radius * radius
